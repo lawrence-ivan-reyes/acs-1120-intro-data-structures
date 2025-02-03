02d22b5dcd64b06628c07e0c1dfa354402b7af10
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-02-03 15:45:13 -0500

    finished listogram.py __contains__ function

--- Code/listogram.py
+++ Code/listogram.py
@@ -38,7 +38,10 @@ class Listogram(list):
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
+        for item in self: # looping through every [word, count] pair again
+            if item[0] == word: # if word is found...
+                return True # return true
+        return False # if we finish loop w/o finding word, return false
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
