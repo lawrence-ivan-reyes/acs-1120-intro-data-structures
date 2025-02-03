cfc43c8cb14aae97ba40d0db2aa526666070b77d
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-02-03 15:41:40 -0500

    finished listogram.py frequency function

--- Code/listogram.py
+++ Code/listogram.py
@@ -31,7 +31,10 @@ class Listogram(list):
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        for item in self: # looping through every [word, count] pair
+            if item[0] == word: # if word is found...
+                return item[1] # return its count
+        return[0] # if word isnt found, return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
